e03dd5e3c7945d897763133fcd157b0de6e2d58f
CayoRoberto <cayoroberto08@gmail.com>
2023-10-05 01:35:16 -0300

    refactor: Alterações controller de usuário e transferência

--- src/main/java/com/picpaysimplificado/controller/TransferenciaController.java
+++ src/main/java/com/picpaysimplificado/controller/TransferenciaController.java
@@ -2,10 +2,11 @@ package com.picpaysimplificado.controller;
 
 
 import com.picpaysimplificado.domain.Transferencia;
-import com.picpaysimplificado.dto.DadosTransferencia;
+import com.picpaysimplificado.dto.TransferenciaDTO;
 import com.picpaysimplificado.service.TransferenciaService;
 import com.picpaysimplificado.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,8 +23,8 @@ public class TransferenciaController {
     private TransferenciaService transferenciaService;
 
     @PostMapping
-    public ResponseEntity<Transferencia> realizarTransferencia(@RequestBody DadosTransferencia dados) throws Exception {
-        transferenciaService.criarTransferencia(dados);
-        return null;
+    public ResponseEntity<Transferencia> realizarTransferencia(@RequestBody TransferenciaDTO dados) throws Exception {
+        Transferencia transferencia = transferenciaService.criarTransferencia(dados);
+        return new ResponseEntity<>(transferencia, HttpStatus.OK);
     }
 }
--- src/main/java/com/picpaysimplificado/controller/UsuarioController.java
+++ src/main/java/com/picpaysimplificado/controller/UsuarioController.java
@@ -2,15 +2,14 @@ package com.picpaysimplificado.controller;
 
 
 import com.picpaysimplificado.domain.Usuario;
-import com.picpaysimplificado.dto.DadosUsuario;
+import com.picpaysimplificado.dto.UsuarioDTO;
 import com.picpaysimplificado.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -19,8 +18,15 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
     @PostMapping
-    public ResponseEntity<Usuario> cadastrar(@RequestBody DadosUsuario dados){
+    public ResponseEntity<Usuario> cadastrar(@RequestBody UsuarioDTO dados){
         Usuario novoUsuario = usuarioService.cadastrar(dados);
         return new ResponseEntity<>(novoUsuario, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarTodosUsuarios(){
+        List<Usuario> usuarios = this.usuarioService.pegarTodosUsuarios();
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+
     }
 }
